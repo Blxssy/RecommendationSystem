7b5563f2cf24aa8485c7774a443ef2e17d3bc984
bjalyona <aly0nasm@mail.ru>
2025-03-04 23:23:55 +0300

    coisine similarity optimization

--- second_model.py
+++ second_model.py
@@ -20,11 +20,6 @@ tfidf_vectorizer = TfidfVectorizer(stop_words="english")
 tfidf_matrix = tfidf_vectorizer.fit_transform(data["combined_features"])
 #print(tfidf_matrix.shape)
 
-# calculating Cosine Similarity
-cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
-cosine_sim_df = pd.DataFrame(cosine_sim, index=data.index, columns=data.index)
-#print(cosine_sim_df)
-
 end_time = time.time()  # Засекаем время окончания выполнения
 elapsed_time = end_time - start_time
 
@@ -33,22 +28,20 @@ print(f"Finish preparing. Execution time: {elapsed_time:.2f} seconds\n")
 def normalize_title(movie_title):
     return re.sub(r'\s+', '', movie_title).lower()
 
-def get_movies(movie_title, cosine_sim=cosine_sim_df, num_recommendations=5):
+def get_movies(movie_title, num_recommendations=5):
     normalized_title = normalize_title(movie_title)
-    matching_titles = {normalize_title(title): title for title in df.index}
+    matching_titles = {normalize_title(title): i for i, title in enumerate(df[first_column])}
+
     if normalized_title not in matching_titles:
         return ["The movie was not found"]
 
-    original_title = matching_titles[normalized_title]
-    # получаем список похожих фильмов, сортируем по убыванию схожести
-    similar_movies = cosine_sim[original_title].sort_values(ascending=False)
-    # убираем сам фильм из списка рекомендаций
-    similar_movies = similar_movies.iloc[1:]
+    idx = matching_titles[normalized_title]
 
-    base_name = re.sub(r'[:\-—\d]', '', original_title).strip().lower()
-    # попытка отсортировать сиквелы
-    filtered_movies = [
-        movie for movie in similar_movies.index
-        if base_name not in movie.lower()
-    ]
-    return [df.loc[movie, first_column] for movie in filtered_movies[:num_recommendations]]
+    query_vector = tfidf_matrix[idx] # вектор конкретного фильма
+
+    # вычисляем Cosine Similarity
+    cosine_similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()
+    # получаем индексы 5 самых похожих фильмов
+    similar_indices = cosine_similarities.argsort()[-(num_recommendations + 1):-1][::-1]
+
+    return df.iloc[similar_indices][first_column].tolist()
